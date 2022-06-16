6b7590e2c35234d210a61252d8f43199866619df
Pranay-03 <84457467+Pranay-03@users.noreply.github.com>
2022-06-16 14:22:49 +0530

    Update knapsack0\1.c

--- DAA/knapsack0\1.c
+++ DAA/knapsack0\1.c
@@ -10,20 +10,15 @@ int knapsack(int W, int wt[], int val[], int n) {
    int i, w;
    int knap[n+1][W+1];
    
-   for (i = 0; i <= n; i++) {
-      
+   for (i = 0; i <= n; i++) 
 	  for (w = 0; w <= W; w++) {
-      
-	     if (i==0 || w==0)
-            knap[i][w] = 0;
-         
-		 else if (wt[i-1] <= w)
-            knap[i][w] = max(val[i-1] + knap[i-1][w-wt[i-1]], knap[i-1][w]);
-         
-		 else
-            knap[i][w] = knap[i-1][w];
+	     	if (i==0 || w==0)
+            		knap[i][w] = 0;
+		else if (wt[i-1] <= w)
+            		knap[i][w] = max(val[i-1] + knap[i-1][w-wt[i-1]], knap[i-1][w]);
+		else
+            		knap[i][w] = knap[i-1][w];
       }
-   }
    return knap[n][W];
 }
 int main() 
@@ -44,11 +39,9 @@ int main()
 	    printf("\nEnter the weight of %d :",i+1);
 	    scanf("%d",&wt[i]);
 	}
-	printf("\n");
-		
+	printf("\n");	
 	printf("Enter the capacity :");
 	scanf("%d",&w);
-
     printf("The solution is : %d", knapsack(w, wt, val, n));
     return 0;
 }
